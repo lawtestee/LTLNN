99ccd3ac3b2e4a5eda6b844d31a27074b65e1eb9
behter <behter@mail.ru>
2019-04-01 18:26:00 +0300

    Update Smart Contract

---
+++ contracts/ERC20.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.6;
+
+
+/**
+ * @title ERC20 interface
+ */
+interface ERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address who) external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+}
---
+++ contracts/Owner.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.6;
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address payable private _owner;
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor() public {
+        _owner = msg.sender;
+    }
+
+    /**
+     * @return the address of the owner.
+     */
+    function owner() public view returns(address payable) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == msg.sender);
+        _;
+    }
+}
